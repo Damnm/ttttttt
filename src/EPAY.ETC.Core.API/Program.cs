1c468f1489cf58c97be96f468ef8fa7aabd4c6d0
Lê Xuân Hà <halx@etc.vn>
2023-08-11 11:06:24 +0700

    Init structure project.

---
+++ src/EPAY.ETC.Core.API/Program.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using NLog;
+using NLog.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// Configuration for swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "EPAY ETC Core API",
+        Version = "v1"
+    });
+    // To Enable authorization using Swagger (JWT)    
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+
+                    }
+                });
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "EPAYPolicy",
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
+
+// NLog: Setup NLog for Dependency injection
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+builder.Host.UseNLog();
+
+
+
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Configuration for NLog
+var configuringNLogFileName = "nlog.config";
+var configuringNLogEnvironmentFileName = $"nlog.{app.Environment.EnvironmentName}.config";
+
+if (File.Exists(configuringNLogEnvironmentFileName))
+{
+    configuringNLogFileName = configuringNLogEnvironmentFileName;
+}
+
+LogManager.Setup().LoadConfigurationFromFile(configuringNLogFileName);
+
+// Configuration for swagger
+_ = app.UseSwagger();
+_ = app.UseSwaggerUI(c =>
+{
+    if (builder.Environment.IsDevelopment()) // config to save authorization when restart api on dev env
+    {
+        c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+    }
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EPAY ETC Core API");
+});
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
