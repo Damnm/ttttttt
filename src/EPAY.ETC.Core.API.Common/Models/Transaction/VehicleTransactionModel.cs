a8237c5d3648081d10cd1f7649e32271af08e2a7
Nghiêm Trịnh Hưng Thịnh <thinhnth@epayjsc.com>
2023-08-15 14:34:06 +0700

    init commit

---
+++ src/EPAY.ETC.Core.API.Common/Models/Transaction/VehicleTransactionModel.cs
@@ -0,0 +1,31 @@
+﻿using EPAY.ETC.Core.API.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAY.ETC.Core.API.Core.Models.Transaction
+{
+    public class VehicleTransactionModel : BaseEntity<Guid>
+    {
+        
+        public Guid LaneInId { get; set; }
+        public DateTime LaneInDate { get; set; }
+        public Guid LaneOutId { get; set; }
+        public DateTime LaneOutDate { get; set; }
+        public int Duration { get; set; }
+        public string? PlateNumber { get; set; }
+        public string? RFID { get; set; }
+        public string? LaneInPlateNumberPhotoURL { get; set; }
+        public string? LaneInVehiclePhotoURL { get; set; }
+        public string? LaneOutPlateNumberPhotoURL { get; set; }
+        public string? LaneOutVehiclePhotoURL { get; set; }
+        public string? PaymentMethod { get; set; }
+        public double Amount { get; set; }
+        public Guid TicketId { get; set; }
+        public Guid ExternalEmployeeId { get; set; }
+        public Guid ShiftId { get; set; }
+        public Guid VehicleId { get; set; }
+    }
+}
--- src/EPAY.ETC.Core.API.Common/Models/Vehicle/VehicleModel.cs
+++ src/EPAY.ETC.Core.API.Common/Models/Vehicle/VehicleModel.cs
@@ -3,11 +3,12 @@
 namespace EPAY.ETC.Core.API.Core.Models.Vehicle
 {
     public class VehicleModel : BaseEntity<Guid>
-    {
+    { 
+        public string? RFID { get; set; }   
         public string? PlateNumber { get; set; }
         public string? PlateColor { get; set; }
         public string? Make { get; set; }
-        public string? Seat { get; set; }
+        public int Seat { get; set; }
         public int Weight { get; set; }
         /// <summary>
         /// TODO: Check data type: use enum instead of string
