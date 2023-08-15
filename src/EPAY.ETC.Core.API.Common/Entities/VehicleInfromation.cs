f86c6abd40e8a8930ce7b7ff6899d68df19b7343
Nghiêm Trịnh Hưng Thịnh <thinhnth@epayjsc.com>
2023-08-15 16:22:08 +0700

    init commit

---
+++ src/EPAY.ETC.Core.API.Common/Entities/VehicleInfromation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAY.ETC.Core.API.Core.Entities
+{
+    public class VehicleInfromation
+    {
+    }
+}
---
+++ src/EPAY.ETC.Core.API.Common/Extensions/DateTimeHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAY.ETC.Core.API.Core.Extensions
+{
+    public static class DateTimeHelperExtensions
+    {
+        /// <summary>
+        /// Converts the date and time to SE Asia Standard Time (+7:00)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTime ConvertToAsianTime(this DateTime dt, DateTimeKind dateTimeKind)
+        {
+            dt = DateTime.SpecifyKind(dt, dateTimeKind);
+            return System.TimeZoneInfo.ConvertTime(dt, TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time"));
+        }
+    }
+}
