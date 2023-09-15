e70d6014ad535e531ae077fd485fa7204647b240
Nghiêm Trịnh Hưng Thịnh <thinhnth@epayjsc.com>
2023-09-15 08:49:02 +0700

    update

---
+++ src/EPAY.ETC.Core.API.Infrastructure/Migrations/20230915011026_dbtest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPAY.ETC.Core.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbtest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
