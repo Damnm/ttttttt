12d071b205715b7954c057c4aff2cd98cfb72612
Trần Mạnh Quốc <quoctm@epayjsc.com>
2023-09-27 09:29:19 +0700

    init IUIActionService

---
+++ src/EPAY.ETC.Core.API.Common/Interfaces/Services/UIActions/IUIActionService.cs
@@ -0,0 +1,14 @@
+﻿using EPAY.ETC.Core.Models.Devices;
+using EPAY.ETC.Core.Models.Fees;
+using EPAY.ETC.Core.Models.Receipt.SessionReports;
+
+namespace EPAY.ETC.Core.API.Core.Interfaces.Services.UIActions
+{
+    public interface IUIActionService
+    {
+        Task<SessionReceiptModel> PrintLaneSessionReport(SessionReceiptRequestModel input);
+        Task CreateDataInput();
+        Task<PaymenStatusResponseModel> UpdatePaymentMethod(PaymentStatusModel input);
+        Task<BarrierModel> ManipulateBarrier(BarrierModel input);
+    }
+}
