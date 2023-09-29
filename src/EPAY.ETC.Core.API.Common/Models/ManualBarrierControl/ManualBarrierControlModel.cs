59af90015dc0763376e5265a8702d949b0873800
Nghiêm Trịnh Hưng Thịnh <thinhnth@epayjsc.com>
2023-09-29 09:12:59 +0700

    updat barriercontrol model

--- src/EPAY.ETC.Core.API.Common/Models/ManualBarrierControl/ManualBarrierControlModel.cs
+++ src/EPAY.ETC.Core.API.Common/Models/ManualBarrierControl/ManualBarrierControlModel.cs
@@ -6,7 +6,7 @@ namespace EPAY.ETC.Core.API.Core.Models.ManualBarrierControl
     [Table("ManualBarrierControl")]
     public class ManualBarrierControlModel : BaseEntity<Guid>
     {
-        public Guid? EmployeeId { get; set; }
+        public string? EmployeeId { get; set; }
         public ActionEnum Action { get; set; }
         public string LaneOutId { get; set; }
     }
