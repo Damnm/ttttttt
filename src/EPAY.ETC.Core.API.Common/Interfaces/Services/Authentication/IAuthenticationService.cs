d00278e55d1c74c9ce360ffde379e430be8375ab
Nghiêm Trịnh Hưng Thịnh <thinhnth@epayjsc.com>
2023-10-04 17:01:48 +0700

    init AuthenticatedEmployee

---
+++ src/EPAY.ETC.Core.API.Common/Interfaces/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using EPAY.ETC.Core.Models.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAY.ETC.Core.API.Core.Interfaces.Services.Authentication
+{
+    public interface IAuthenticationService<T>
+    {
+        Task<ValidationResult<T>> AuthenticateAsync(string employeeId, string pwd);
+        Task<ValidationResult<T>> AutoAuthenticateAsync(string employeeId, string actionCode);
+    }
+}
