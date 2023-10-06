353991daa660295e568cf0053fa779de501b2854
Trần Mạnh Quốc <quoctm@epayjsc.com>
2023-10-06 09:50:11 +0700

    Update

---
+++ src/UnitTests/EPAY.ETC.Core.API.UnitTests/Controllers/UIAction/UIActionController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAY.ETC.Core.API.UnitTests.Controllers.UIActions
+{
+    public class UIActionController
+    {
+        #region Init instance mock
+        #endregion
+    }
+}
