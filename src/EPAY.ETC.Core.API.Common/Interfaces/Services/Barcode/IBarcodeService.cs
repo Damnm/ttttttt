468e8e456ac951cfcdbb65b25d781eab8111d2e0
Trần Mạnh Quốc <quoctm@epayjsc.com>
2023-10-11 14:55:15 +0700

    Add get barcode info End-point

--- src/EPAY.ETC.Core.API.Common/Interfaces/Services/Barcode/IBarcodeService.cs
+++ src/EPAY.ETC.Core.API.Common/Interfaces/Services/Barcode/IBarcodeService.cs
@@ -1,6 +1,7 @@
 ﻿using EPAY.ETC.Core.API.Core.Models.Barcode;
 using EPAY.ETC.Core.Models.Request;
 using EPAY.ETC.Core.Models.Validation;
+using System.Linq.Expressions;
 
 namespace EPAY.ETC.Core.API.Core.Interfaces.Services.Barcode
 {
@@ -10,5 +11,6 @@ namespace EPAY.ETC.Core.API.Core.Interfaces.Services.Barcode
         public Task<ValidationResult<BarcodeModel>> GetByIdAsync(Guid id);
         public Task<ValidationResult<BarcodeModel>> UpdateAsync(Guid id, BarcodeAddOrUpdateRequestModel request);
         public Task<ValidationResult<BarcodeModel>> RemoveAsync(Guid id);
+        public Task<ValidationResult<List<BarcodeModel>>> GetListAsync(Expression<Func<BarcodeModel, bool>>? expression);
     }
 }
