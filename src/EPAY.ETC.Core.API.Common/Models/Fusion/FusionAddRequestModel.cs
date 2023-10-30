aaac367fc12936ca4264ab0cae1ccae779677274
Mạnh Quốc <quoctm@epayjsc.com>
2023-10-30 15:56:31 +0700

    Update FusionAddRequestModel remove required field

--- src/EPAY.ETC.Core.API.Common/Models/Fusion/FusionAddRequestModel.cs
+++ src/EPAY.ETC.Core.API.Common/Models/Fusion/FusionAddRequestModel.cs
@@ -6,19 +6,15 @@ namespace EPAY.ETC.Core.API.Core.Models.Fusion
     [ExcludeFromCodeCoverage]
     public class FusionAddRequestModel
     {
-        
         //public Guid ObjectId { get; set; }
         public long Epoch { get; set; }
         public bool Loop1 { get; set; }
         [MaxLength(50)]
-        [Required(ErrorMessage = "PlateNumber required!")]
         public string? RFID { get; set; }
         [MaxLength(15)]
-        [Required(ErrorMessage = "PlateNumber required!")]
         public string? Cam1 { get; set; }
         public bool Loop2 { get; set; }
         [MaxLength(15)]
-        [Required(ErrorMessage = "PlateNumber required!")]
         public string? Cam2 { get; set; }
         public bool Loop3 { get; set; }
         public bool ReversedLoop1 { get; set; }
