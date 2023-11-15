77b8417df3c44ee19cfca83b9bfe7407441d1a5d
Lê Xuân Hà <halx@etc.vn>
2023-11-15 15:43:35 +0700

    Add UnitTest for PrintLog

---
+++ src/EPAY.ETC.Core.API.Common/Interfaces/Services/PrintLog/IPrintLogService.cs
@@ -0,0 +1,14 @@
+﻿using EPAY.ETC.Core.API.Core.Models.PrintLog;
+using EPAY.ETC.Core.Models.Request;
+using EPAY.ETC.Core.Models.Validation;
+
+namespace EPAY.ETC.Core.API.Core.Interfaces.Services.PrintLog
+{
+    public interface IPrintLogService
+    {
+        Task<ValidationResult<PrintLogModel>> AddAsync(PrintLogRequestModel input);
+        Task<ValidationResult<PrintLogModel>> GetByIdAsync(Guid id);
+        Task<ValidationResult<PrintLogModel>> UpdateAsync(Guid id , PrintLogRequestModel input);
+        Task<ValidationResult<PrintLogModel>> RemoveAsync(Guid id);
+    }
+}
---
+++ src/EPAY.ETC.Core.API.Infrastructure/Persistence/Repositories/PrintLog/IPrintLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using EPAY.ETC.Core.API.Core.Interfaces.Repositories;
+using EPAY.ETC.Core.API.Core.Models.PrintLog;
+using EPAY.ETC.Core.API.Core.Models.Vehicle;
+
+namespace EPAY.ETC.Core.API.Infrastructure.Persistence.Repositories.PrintLog
+{
+    public interface IPrintLogRepository : IRepository<PrintLogModel, Guid>
+    {
+
+    }
+}
