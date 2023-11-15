512357001057e3d6d1fe5ebf16a873c302592a0d
Lê Xuân Hà <halx@etc.vn>
2023-11-15 10:17:37 +0700

    Add Unit Test

---
+++ src/UnitTests/EPAY.ETC.Core.API.Infrastructure.UnitTests/Services/TransactionLog/LaneInCameraTransactionLogTests.cs
@@ -0,0 +1,86 @@
+﻿using EPAY.ETC.Core.API.Core.Models.TransactionLog;
+using EPAY.ETC.Core.API.Infrastructure.Persistence.Repositories.TransactionLog;
+using EPAY.ETC.Core.API.Infrastructure.Services.TransactionLog;
+using EPAY.ETC.Core.API.Infrastructure.UnitTests.Common;
+using EPAY.ETC.Core.API.Infrastructure.UnitTests.Helpers;
+using EPAY.ETC.Core.Models.Request;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Linq.Expressions;
+
+namespace EPAY.ETC.Core.API.Infrastructure.UnitTests.Services.TransactionLog
+{
+    public class LaneInCameraTransactionLogTests : AutoMapperTestBase
+    {
+        #region Init mock data
+        private readonly Exception _exception = null!;
+        private readonly Mock<ILogger<LaneInCameraTransactionLogService>> _loggerMock = new();
+        private readonly Mock<ILaneInCameraTransactionLogRepository> _repositoryMock = new();
+        private List<LaneInCameraTransactionLogRequest> laneInCameraTransactionLog = new List<LaneInCameraTransactionLogRequest>();
+       
+        private LaneInCameraTransactionLogRequest request =
+              new LaneInCameraTransactionLogRequest()
+              {
+                  PlateNumber = "Some Plate number",
+                  RFID = "Some RFID",
+                  Make = "Some make",
+                  Seat = 10,
+                  VehicleType = "Loại 2",
+                  Weight = 7000,
+
+              };
+
+        #endregion
+
+        #region UpdateInsertAsync
+        [Fact]
+        public async Task GivenValidRequest_WhenUpdateInsertAsyncIsCalled_ThenReturnCorrectResult()
+        {
+            // Arrange
+            // Arrange
+            Expression<Func<LaneInCameraTransactionLog, bool>> expression = s => s.Make == string.Empty;
+            _repositoryMock.Setup(x => x.GetAllAsync(It.IsAny<Expression<Func<LaneInCameraTransactionLog, bool>>>())).ReturnsAsync(new List<LaneInCameraTransactionLog>());
+            _repositoryMock.Setup(x => x.AddAsync(It.IsNotNull<LaneInCameraTransactionLog>())).ReturnsAsync(new LaneInCameraTransactionLog() {
+                PlateNumber = "Some Plate number",
+                RFID = "Some RFID",
+                Make = "Some make",
+                Seat = 10,
+                VehicleType = "Loại 2",
+                Weight = 7000,
+            });
+
+            // Act
+            var service = new LaneInCameraTransactionLogService(_loggerMock.Object, _repositoryMock.Object, _mapper);
+            var result = await service.UpdateInsertAsync(request);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Data.Should().BeTrue();
+            result.Succeeded.Should().BeTrue();
+            _repositoryMock.Verify(x => x.GetAllAsync(It.IsAny<Expression<Func<LaneInCameraTransactionLog, bool>>>()), Times.Once);
+            _repositoryMock.Verify(x => x.AddAsync(It.IsNotNull<LaneInCameraTransactionLog>()), Times.Once);
+            _loggerMock.VerifyLog(LogLevel.Information, $"Executing {nameof(service.UpdateInsertAsync)} method...", Times.Once, _exception);
+            _loggerMock.VerifyLog(LogLevel.Error, $"Failed to run {nameof(service.UpdateInsertAsync)} method", Times.Never, _exception);
+        }
+
+        [Fact]
+        public async Task GivenValidRequestAndRepositoryIsDown_WhenAddAsyncIsCalled_ThenThrowException()
+        {
+            // Arrange
+            _repositoryMock.Setup(x => x.GetAllAsync(It.IsAny<Expression<Func<LaneInCameraTransactionLog, bool>>>())).ThrowsAsync(new Exception());
+
+            // Act
+            var service = new LaneInCameraTransactionLogService(_loggerMock.Object, _repositoryMock.Object, _mapper);
+            Func<Task> func = () => service.UpdateInsertAsync(request);
+
+            // Assert
+            var ex = await Assert.ThrowsAsync<Exception>(func);
+            _repositoryMock.Verify(x => x.GetAllAsync(It.IsAny<Expression<Func<LaneInCameraTransactionLog, bool>>>()), Times.Once);
+            _loggerMock.VerifyLog(LogLevel.Information, $"Executing {nameof(service.UpdateInsertAsync)} method...", Times.Once, _exception);
+            _loggerMock.VerifyLog(LogLevel.Error, $"Failed to run {nameof(service.UpdateInsertAsync)} method", Times.Once, _exception);
+        }
+        #endregion
+
+    }
+}
---
+++ src/UnitTests/EPAY.ETC.Core.API.UnitTests/Controllers/TransactionLog/LaneInCameraTransactionLogControllerTests.cs
@@ -0,0 +1,89 @@
+﻿using EPAY.ETC.Core.API.Controllers.TransactionLog;
+using EPAY.ETC.Core.API.Core.Interfaces.Services.Transaction;
+using EPAY.ETC.Core.API.Core.Models.Vehicle;
+using EPAY.ETC.Core.API.UnitTests.Helpers;
+using EPAY.ETC.Core.Models.Request;
+using EPAY.ETC.Core.Models.Validation;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+namespace EPAY.ETC.Core.API.UnitTests.Controllers.TransactionLog
+{
+    public class LaneInCameraTransactionLogControllerTests : ControllerBase
+    {
+        private readonly Exception _exception = null!;
+        private Mock<ILaneInCameraTransactionLogService> _laneInCameraTransactionLogServiceMock = new Mock<ILaneInCameraTransactionLogService>();
+        private Mock<ILogger<LaneInCameraTransactionLogController>> _loggerMock = new Mock<ILogger<LaneInCameraTransactionLogController>>();
+        private VehicleModel requestMock = new VehicleModel()
+        {
+            PlateNumber = "Some Plate number",
+            PlateColor = "Some Plate colour",
+            RFID = "Some RFID",
+            Make = "Some make",
+            Seat = 10,
+            VehicleType = "Loại 2",
+            Weight = 7000,
+        };
+        private ValidationResult<VehicleModel> responseMock = new ValidationResult<VehicleModel>(new VehicleModel()
+        {
+            Id = new Guid(),
+            CreatedDate = DateTime.Now,
+            PlateNumber = "Some Plate number",
+            PlateColor = "Some Plate colour",
+            RFID = "Some RFID",
+            Make = "Some make",
+            Seat = 10,
+            VehicleType = "Loại 2",
+            Weight = 7000,
+        });
+
+        #region UpdateInsertAsync
+       
+        [Fact]
+        public async Task GivenValidRequest_WhenApiUpdateInsertAsyncIsCalled_ThenReturnCorrectResult()
+        {
+            // Arrange
+            _laneInCameraTransactionLogServiceMock.Setup(x => x.UpdateInsertAsync(It.IsAny<LaneInCameraTransactionLogRequest>())).ReturnsAsync(ValidationResult.Success(true));
+            var request = new VehicleModel();
+
+            // Act
+            var vehicleController = new LaneInCameraTransactionLogController(_loggerMock.Object
+                , _laneInCameraTransactionLogServiceMock.Object);
+            var actualResult = await vehicleController.UpdateInsertAsync(It.IsAny<LaneInCameraTransactionLogRequest>());
+            var data = ((ObjectResult)actualResult).Value as ValidationResult<VehicleModel>;
+
+            // Assert
+            _loggerMock.VerifyLog(LogLevel.Information, $"Executing {nameof(vehicleController.UpdateInsertAsync)}...", Times.Once, _exception);
+            _loggerMock.VerifyLog(LogLevel.Error, $"An error occurred when calling {nameof(vehicleController.UpdateInsertAsync)} method", Times.Never, _exception);
+            actualResult.Should().BeOfType<ObjectResult>();
+            ((ObjectResult)actualResult).StatusCode.Should().Be(StatusCodes.Status201Created);
+            data?.Succeeded.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GivenValidRequestAndVehicleServiceIsDown_WhenApiUpdateInsertAsyncIsCalled_ThenReturnInternalServerError()
+        {
+            // Arrange
+            var someEx = new Exception("An error occurred when calling AddAsync method");
+            _laneInCameraTransactionLogServiceMock.Setup(x => x.UpdateInsertAsync(It.IsAny<LaneInCameraTransactionLogRequest>())).ThrowsAsync(someEx);
+
+            // Act
+            var vehicleController = new LaneInCameraTransactionLogController(_loggerMock.Object
+                , _laneInCameraTransactionLogServiceMock.Object);
+            var actualResult = await vehicleController.UpdateInsertAsync(It.IsAny<LaneInCameraTransactionLogRequest>());
+            var actualResultRespone = ((ObjectResult)actualResult).Value as ValidationResult<string>;
+
+            // Assert
+            _loggerMock.VerifyLog(LogLevel.Information, $"Executing {nameof(vehicleController.UpdateInsertAsync)}...", Times.Once, _exception);
+            _loggerMock.VerifyLog(LogLevel.Error, $"An error occurred when calling {nameof(vehicleController.UpdateInsertAsync)} method", Times.Once, _exception);
+            ((ObjectResult)actualResult).StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+            actualResultRespone!.Succeeded.Should().BeFalse();
+            Assert.True(actualResultRespone.Errors.Count() > 0);
+        }
+        #endregion
+    }
+}
