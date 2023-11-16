227d63813cbdde972a6aa50c8d38c6712b24d813
Trần Mạnh Quốc <quoctm@epayjsc.com>
2023-11-16 15:55:59 +0700

    fix bug

--- src/EPAY.ETC.Core.API.Infrastructure/Services/Fees/FeeCalculationService.cs
+++ src/EPAY.ETC.Core.API.Infrastructure/Services/Fees/FeeCalculationService.cs
@@ -192,7 +192,7 @@ namespace EPAY.ETC.Core.API.Infrastructure.Services.Fees
                     x.PlateNumber != null
                     && !string.IsNullOrEmpty(plateNumber)
                     && x.PlateNumber.Equals(plateNumber)
-                    && x.ValidFrom >= checkOutDateTime && (x.ValidTo == null || x.ValidTo >= checkOutDateTime)
+                    && x.ValidFrom <= checkOutDateTime && (x.ValidTo == null || x.ValidTo >= checkOutDateTime)
                 );
                 var feeVehicleCategory = feeVehicleCategories.OrderBy(x => x.PlateNumber).FirstOrDefault();
 
